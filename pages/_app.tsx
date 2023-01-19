0a1d6d9df9ce55e0443f0119fe72395704913de7
Luis Cisneros <ext_lucisneros@falabella.cl>
2023-01-19 15:59:22 -0500

    build: install and config react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import '@/styles/globals.css';
 
 import Layout from '@/components/Layout';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-import type { AppProps } from 'next/app'
-
+import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
